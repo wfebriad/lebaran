e746f68c0f6e295bad21a6ccd0950aaac5de425d
Wisnu Febriadi <idtfc.febriady@gmail.com>
2020-05-23 22:37:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@
 // makeItRain();
 
 new Typed('#typed',{
-        strings : ['Selamat Hari Raya Idul Fitri 1441 H.' \n ' Mohon maaf lahir dan batin.'],
+        strings : ['Selamat Hari Raya Idul Fitri 1441 H./\n Mohon maaf lahir dan batin.'],
         typeSpeed : 70,
         delaySpeed : 70,
         loop : true
